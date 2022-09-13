9cc443e6e3ca91cc380150692871c204edec5791
WestHorizon <zzr@bupt.edu.cn>
2022-09-13 15:05:36 +0800

    2.3 字符串

---
+++ Unit2/string.py
@@ -0,0 +1,24 @@
+name = "ada lovelace"
+print("==> name:", name)
+
+print("==> name.title():", name.title())
+print("==> name.upper():", name.upper())
+print("==> name.lower():", name.lower())
+
+name = "aDa lOvELace"
+print("==> name:", name)
+print("==> name.tile():", name.title())
+
+
+first_name = "ada"
+last_name = "lovelace"
+# After py3.6
+full_name = f"{first_name} {last_name}"
+print("==> full_name:", full_name)
+print(f"Hello,{full_name}!")
+message = f"Hello,{full_name}!"
+print("==> message:", message)
+
+# Before py3.5
+full_name = "{} {}".format(first_name, last_name)
+print("==> full_name:", full_name)
